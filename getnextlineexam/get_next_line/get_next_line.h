e0bec33dc86d80fa2fe1e6b1b8cf9f193d14d9f9
kamalmoundir <moundirkamal@gmail.com>
2025-05-22 18:24:46 +0200

    add exam

---
+++ getnextlineexam/get_next_line/get_next_line.h
@@ -0,0 +1,16 @@
+
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#ifndef BUFFER_SIZE
+# define  BUFFER_SIZE 42
+
+#endif
+
+#endif
